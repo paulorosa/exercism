6745e610af7cb9af9b3af6578918b5d25bdda432
Paulo Rosa <paulo.rosa@gmail.com>
2019-03-18 01:01:21 +0000

    robot-name exercise

---
+++ python/robot-name/robot_name.py
@@ -0,0 +1,9 @@
+class Robot(object):
+    def __init__(self):
+        self.name = "AB123"
+    def reset(self):
+        self.name = "AD123" 
+    def seed(seed):
+        self.name = "CB123"
+    def name(self):
+        return "CF123"
