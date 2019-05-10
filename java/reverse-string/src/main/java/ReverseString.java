4918eaa89141d9ead0828e3c644d13bd7fb9f048
EC2 Default User <ec2-user@ip-172-31-58-162.ec2.internal>
2019-05-10 00:18:19 +0000

    okx

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,13 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String reverse = "";
+        
+        for(int i = inputString.length() - 1; i >= 0; i--)
+        {
+            reverse = reverse + inputString.charAt(i);
+        }
+        return reverse;
     }
   
 }
